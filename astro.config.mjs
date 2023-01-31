a8a715705465094837c47deb6751e7ec07512c44
Slava <urigami2010@gmail.com>
2023-01-31 15:08:19 +0200

    init commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import image from "@astrojs/image";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://SlavaDemo.github.io',
-  base: '/SlavaDemo',
+  // base: '/SlavaDemo',
   integrations: [image(
     {serviceEntryPoint: '@astrojs/image/sharp'}
   )]
---
+++ src/data/data.js
@@ -0,0 +1,81 @@
+const languages = [
+    {
+        name:"Python",
+        image: "python.svg"
+    },
+    {
+        name:"Dart",
+        image: "dart.svg"
+    },
+    {
+        name:"JavaScript",
+        image: "javascript.svg"
+    },
+    {
+        name:"TypeScript",
+        image: "typescript.svg"
+    },
+    {
+        name:"HTML",
+        image: "html.svg"
+    },
+    {
+        name:"CSS",
+        image: "css.svg"
+    },
+    {
+        name:"SaSS",
+        image: "sass.svg"
+    },
+    {
+        name:"SQL",
+        image: "sql.svg"
+    },
+    {
+        name:"NoSQL",
+        image: "nosql.svg"
+    },
+    {
+        name:"Java",
+        image: "java.svg"
+    },
+]
+
+const frameworks = [
+    {
+        name:"Django",
+        image: "django.svg"
+    },
+    {
+        name:"Flask",
+        image: "flask.svg"
+    },
+]
+
+const clouds = [
+    {
+        name:"Azure",
+        image: "azure.svg"
+    },
+    {
+        name:"Firestore",
+        image: "firestore.svg"
+    },
+]
+
+const tools = [
+    {
+        name:"Linux",
+        image: "linux.svg"
+    },
+    {
+        name:"Jenkins",
+        image: "jenkins.svg"
+    },
+    {
+        name:"Docker",
+        image: "docker.svg"
+    },
+]
+
+export {languages,frameworks, clouds,tools};
